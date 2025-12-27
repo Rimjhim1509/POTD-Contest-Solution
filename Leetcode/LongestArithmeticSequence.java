70d0399bbf42a8b49bb77107c761599d60926762
Rimjhim1509 <149322220+Rimjhim1509@users.noreply.github.com>
2025-12-27 14:03:53 +0530

    Implement longest arithmetic sequence solution

---
+++ Leetcode/LongestArithmeticSequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestArithSeqLength(int[] nums) {
+        int n= nums.length;
+        if(n<2)return n;
+        int maxl =1;
+        HashMap<Integer , Integer>[]dp = new HashMap[n];
+        for(int i=0;i<n; i++){
+            dp[i]= new HashMap<>();      
+            
+        }
+        for(int i=1;i<n; i++){
+            for(int j=0; j<i; j++){
+                int diff = nums[i] -nums[j];
+                int prev = dp[j].getOrDefault(diff ,1);
+                int curr = prev +1;
+
+                dp[i].put(diff , curr);
+                maxl = Math.max(maxl , dp[i].get(diff));
+            }
+        }
+        return maxl;
+    }
+}
